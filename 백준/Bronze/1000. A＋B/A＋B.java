aa3f7604b480fec890e95bad46c9ded23b1070b6
Minseok Choi <seok7521@gmail.com>
2026-02-26 20:22:50 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1, n2, n3;
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		n3 = n1 +n2;
+		System.out.println(n3);
+		
+		
+		
+	}
+
+}
