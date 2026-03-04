2d314844c3533af767c44f56ef72ecfb3b460fb8
Minseok Choi <seok7521@gmail.com>
2026-03-04 11:39:38 +0900

    [level 0] Title: 접미사 배열, Time: 4.30 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < my_string.length(); i++) {
+            list.add(my_string.substring(i));
+        }
+        return list.stream().sorted().toArray(String[]::new);
+    }
+}
