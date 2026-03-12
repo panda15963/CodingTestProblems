9f45d18ffb00c8a6371c2537653b7bf99ede234a
Minseok Choi <seok7521@gmail.com>
2026-03-12 21:02:19 +0900

    [level 0] Title: 커피 심부름, Time: 1.33 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] order) {
+        int sum = 0;
+        for (String o : order) {
+            if (o.contains("americano") || o.contains("anything")) {
+                sum += 4500;
+            } else {
+                sum += 5000;
+            }
+        }
+        return sum;
+    }
+}
