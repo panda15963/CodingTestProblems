43e19503204e6f26841c3b1b314e255fe85ba345
Minseok Choi <seok7521@gmail.com>
2026-03-16 09:51:13 +0900

    Time: 21 ms (58.76%), Space: 56.2 MB (59.34%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,30 @@
+var longestPalindrome = function(s) {
+
+    let start = 0
+    let end = 0
+
+    const expand = (l, r) => {
+
+        while (l >= 0 && r < s.length && s[l] === s[r]) {
+            l--
+            r++
+        }
+
+        return r - l - 1
+    }
+
+    for (let i = 0; i < s.length; i++) {
+
+        const len1 = expand(i, i)
+        const len2 = expand(i, i + 1)
+
+        const len = Math.max(len1, len2)
+
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2)
+            end = i + Math.floor(len / 2)
+        }
+    }
+
+    return s.substring(start, end + 1)
+};
