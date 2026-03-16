a15b94ce215d122134feafe2f1540226a7ddb28c
Minseok Choi <seok7521@gmail.com>
2026-03-17 07:25:06 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    const factors = [];
+    for (let i = 2; i <= n; i++) {
+        while (n % i === 0) {
+            factors.push(i);
+            n /= i;
+        }
+    }
+    return [...new Set(factors)].sort((a, b) => a - b);
+}
