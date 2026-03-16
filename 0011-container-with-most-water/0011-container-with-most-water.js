56688f0432cc16f817a60b2e7c37e9801e3e48e6
Minseok Choi <seok7521@gmail.com>
2026-03-16 11:07:19 +0900

    Time: 4 ms (16.08%), Space: 63 MB (66.94%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,23 @@
+var maxArea = function(height) {
+
+    let left = 0
+    let right = height.length - 1
+
+    let max = 0
+
+    while (left < right) {
+
+        const h = Math.min(height[left], height[right])
+        const width = right - left
+
+        max = Math.max(max, h * width)
+
+        if (height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return max
+};
