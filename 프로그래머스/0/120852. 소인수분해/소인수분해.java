be4244513101b969d14df21c86caa07fc620e148
Minseok Choi <seok7521@gmail.com>
2026-03-17 07:24:50 +0900

    [level 0] Title: 소인수분해, Time: 9.69 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return factors.stream().mapToInt(Integer::intValue).distinct().sorted().toArray();
+    }
+}
