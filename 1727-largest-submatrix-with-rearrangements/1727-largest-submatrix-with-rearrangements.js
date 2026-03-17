3fc8f9079e958b9fb3cce6f46b88013034fe79b8
Minseok Choi <seok7521@gmail.com>
2026-03-17 22:00:34 +0900

    Time: 43 ms (72.73%), Space: 75.5 MB (81.82%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.js
@@ -0,0 +1,28 @@
+var largestSubmatrix = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    // 1. height 누적
+    for (let i = 1; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 1) {
+                matrix[i][j] += matrix[i - 1][j];
+            }
+        }
+    }
+
+    let maxArea = 0;
+
+    // 2. 정렬 + 3. 넓이 계산
+    for (let row of matrix) {
+        row.sort((a, b) => b - a);
+
+        for (let j = 0; j < n; j++) {
+            const height = row[j];
+            const width = j + 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+    }
+
+    return maxArea;
+};
