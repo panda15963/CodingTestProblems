b0dd7cad1c8207d4735e537c9e526bc5ea4187f9
Minseok Choi <seok7521@gmail.com>
2026-03-19 06:46:22 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.04 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 한 번만 등장한 문자 - 120896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=java) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.20 ms
+메모리: 68.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 19일 06:45:54
+2026년 03월 19일 06:46:19
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] == 1) {
+                sb.append((char)('a' + i));
+            }
+        }
+        return sb.toString();
+    }
+}
