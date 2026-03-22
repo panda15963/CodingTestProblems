435cfb8bfd5668a185612e4d9e61f2794493c5f8
Minseok Choi <seok7521@gmail.com>
2026-03-23 06:20:51 +0900

    [level 0] Title: 특이한 정렬, Time: 5.82 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/README.md
+++ 프로그래머스/0/120880. 특이한 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특이한 정렬 - 120880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120880) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120880?language=java) 
 
 ### 성능 요약
 
-메모리: 32.6 MB, 시간: 0.11 ms
+메모리: 88.8 MB, 시간: 5.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 23일 06:20:29
+2026년 03월 23일 06:20:49
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        Integer[] arr = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, (a, b) -> {
+            int distA = Math.abs(a - n);
+            int distB = Math.abs(b - n);
+            if (distA == distB) return b - a;
+            return distA - distB;
+        });
+        return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+    }
+}
