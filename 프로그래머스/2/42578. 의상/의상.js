5fd94f917d612ad7dabfcebf11e642314f81e0bf
Minseok Choi <seok7521@gmail.com>
2026-03-24 06:09:38 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,11 @@
+function solution(clothes) {
+    const map = new Map();
+    for (let [_, type] of clothes) {
+        map.set(type, (map.get(type) || 0) + 1);
+    }
+    let answer = 1;
+    for (let count of map.values()) {
+        answer *= (count + 1);
+    }
+    return answer - 1;
+}
