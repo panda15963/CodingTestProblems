d718941fd5d3b3a96fd32fc26c84076316f4beb7
Minseok Choi <seok7521@gmail.com>
2026-03-30 21:18:46 +0900

    Time: 10 ms (25%), Space: 61.2 MB (25%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.ts
@@ -0,0 +1,28 @@
+function checkStrings(s1: string, s2: string): boolean {
+    const even1: number[] = new Array(26).fill(0);
+    const odd1: number[] = new Array(26).fill(0);
+    const even2: number[] = new Array(26).fill(0);
+    const odd2: number[] = new Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        const idx1 = s1.charCodeAt(i) - 97;
+        const idx2 = s2.charCodeAt(i) - 97;
+
+        if (i % 2 === 0) {
+            even1[idx1]++;
+            even2[idx2]++;
+        } else {
+            odd1[idx1]++;
+            odd2[idx2]++;
+        }
+    }
+
+    return isEqual(even1, even2) && isEqual(odd1, odd2);
+}
+
+function isEqual(a: number[], b: number[]): boolean {
+    for (let i = 0; i < 26; i++) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
