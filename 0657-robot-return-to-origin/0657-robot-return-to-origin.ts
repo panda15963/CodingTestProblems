64dca1b37b6fa4bbf89125190ab7b867b9c6c585
Minseok Choi <seok7521@gmail.com>
2026-04-05 09:09:10 +0900

    Time: 7 ms (50%), Space: 58.1 MB (51.09%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.ts
@@ -0,0 +1,13 @@
+function judgeCircle(moves: string): boolean {
+    let x = 0;
+    let y = 0;
+
+    for (const move of moves) {
+        if (move === 'U') y++;
+        else if (move === 'D') y--;
+        else if (move === 'L') x--;
+        else if (move === 'R') x++;
+    }
+
+    return x === 0 && y === 0;
+}
