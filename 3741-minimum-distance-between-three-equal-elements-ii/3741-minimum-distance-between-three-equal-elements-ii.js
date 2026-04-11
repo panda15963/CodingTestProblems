87178d9141776a146f9b21f89771798cedae39cb
Minseok Choi <seok7521@gmail.com>
2026-04-11 10:58:26 +0900

    Time: 357 ms (16.67%), Space: 121.2 MB (16.67%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.js
@@ -0,0 +1,17 @@
+var minimumDistance = function(nums) {
+    const pos = new Map();
+    let ans = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        const x = nums[i];
+        if (!pos.has(x)) pos.set(x, []);
+        const arr = pos.get(x);
+        arr.push(i);
+
+        if (arr.length >= 3) {
+            ans = Math.min(ans, arr[arr.length - 1] - arr[arr.length - 3]);
+        }
+    }
+
+    return ans === Infinity ? -1 : ans * 2;
+};
