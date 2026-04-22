ecce7c6b0d4050ac748dedc19617601a7551059f
Minseok Choi <seok7521@gmail.com>
2026-04-22 21:26:44 +0900

    Time: 20 ms (30%), Space: 54.7 MB (95%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.js
@@ -0,0 +1,33 @@
+/**
+ * Finds all queries that differ from at least one dictionary word by at most 2 characters
+ * @param {string[]} queries - Array of query strings to check
+ * @param {string[]} dictionary - Array of dictionary strings to compare against
+ * @returns {string[]} Array of queries that have at most 2 character differences with at least one dictionary word
+ */
+function twoEditWords(queries, dictionary) {
+    // Get the length of the first query string (assuming all strings have the same length)
+    const stringLength = queries[0].length;
+  
+    // Filter queries based on whether they have at most 2 differences with any dictionary word
+    return queries.filter(queryString => {
+        // Check the current query against each word in the dictionary
+        for (const dictionaryWord of dictionary) {
+            let differenceCount = 0;
+          
+            // Count character differences between query and dictionary word
+            for (let charIndex = 0; charIndex < stringLength; ++charIndex) {
+                if (queryString[charIndex] !== dictionaryWord[charIndex]) {
+                    ++differenceCount;
+                }
+            }
+          
+            // If difference is at most 2, include this query in the result
+            if (differenceCount < 3) {
+                return true;
+            }
+        }
+      
+        // No dictionary word found with at most 2 differences
+        return false;
+    });
+}
