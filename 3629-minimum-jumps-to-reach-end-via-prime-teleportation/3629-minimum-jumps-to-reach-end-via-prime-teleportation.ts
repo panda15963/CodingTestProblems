c19f6df9c79018180c416b4b4bb190002d6b69b3
Minseok Choi <seok7521@gmail.com>
2026-05-08 22:08:15 +0900

    Time: 823 ms (11.11%), Space: 132.9 MB (88.89%) - LeetHub

---
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.ts
@@ -0,0 +1,74 @@
+function minJumps(nums: number[]): number {
+  const n = nums.length;
+
+  let maxEl = 0;
+  const mp: Map<number, number[]> = new Map();
+
+  for (let i = 0; i < n; i++) {
+    if (!mp.has(nums[i])) mp.set(nums[i], []);
+    mp.get(nums[i])!.push(i);
+    maxEl = Math.max(maxEl, nums[i]);
+  }
+
+  const isPrime: boolean[] = new Array(maxEl + 1).fill(true);
+  if (maxEl >= 0) isPrime[0] = false;
+  if (maxEl >= 1) isPrime[1] = false;
+
+  for (let num = 2; num * num <= maxEl; num++) {
+    if (isPrime[num]) {
+      for (let multiple = num * num; multiple <= maxEl; multiple += num) {
+        isPrime[multiple] = false;
+      }
+    }
+  }
+
+  const queue: number[] = [0];
+  let head = 0;
+  const visited: boolean[] = new Array(n).fill(false);
+  visited[0] = true;
+
+  const seen: Set<number> = new Set();
+  let steps = 0;
+
+  while (head < queue.length) {
+    const size = queue.length - head;
+
+    for (let s = 0; s < size; s++) {
+      const i = queue[head++];
+
+      if (i === n - 1) return steps;
+
+      if (i - 1 >= 0 && !visited[i - 1]) {
+        visited[i - 1] = true;
+        queue.push(i - 1);
+      }
+
+      if (i + 1 < n && !visited[i + 1]) {
+        visited[i + 1] = true;
+        queue.push(i + 1);
+      }
+
+      if (!isPrime[nums[i]] || seen.has(nums[i])) {
+        continue;
+      }
+
+      for (let multiple = nums[i]; multiple <= maxEl; multiple += nums[i]) {
+        const list = mp.get(multiple);
+        if (!list) continue;
+
+        for (const j of list) {
+          if (!visited[j]) {
+            visited[j] = true;
+            queue.push(j);
+          }
+        }
+      }
+
+      seen.add(nums[i]);
+    }
+
+    steps++;
+  }
+
+  return steps;
+}
