3f60760a4d7ec436c8cb47a8b66113e15d64af62
Minseok Choi <seok7521@gmail.com>
2026-05-23 09:02:14 +0900

    Time: 0 ms (100%), Space: 55.4 MB (48.33%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
@@ -0,0 +1,16 @@
+function check(nums: number[]): boolean {
+    let count = 0;
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
