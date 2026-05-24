802cd306d4fdeee49911536013ef916a9791cfc3
Minseok Choi <seok7521@gmail.com>
2026-05-24 17:34:05 +0900

    [level 0] Title: a와 b 출력하기, Time: 38.39 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/README.md
+++ 프로그래머스/0/181951. a와 b 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] a와 b 출력하기 - 181951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951?language=javascript) 
 
 ### 성능 요약
 
-메모리: 61.3 MB, 시간: 113.21 ms
+메모리: 31.3 MB, 시간: 38.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 24일 17:32:55
+2026년 05월 24일 17:34:03
 
 ### 문제 설명
 
--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -1,13 +1,20 @@
 const readline = require('readline');
+
+// 입력을 위한 인터페이스 생성
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let input = [];
-
 rl.on('line', function (line) {
-    input = line.split(' ');
+    // 입력받은 문자열을 공백으로 구분하여 배열로 변환
+    const input = line.split(' ');
+    const a = Number(input[0]);
+    const b = Number(input[1]);
+    
+    // a와 b 값 출력
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
 }).on('close', function () {
-    console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+    process.exit();
 });
