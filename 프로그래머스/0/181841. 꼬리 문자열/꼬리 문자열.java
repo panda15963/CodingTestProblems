e8622314bcb660b7c7e16e82ec1e92ea1d8b3a65
Minseok Choi <seok7521@gmail.com>
2026-06-08 06:44:57 +0900

    [level 0] Title: 꼬리 문자열, Time: 3.19 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list)
+                .filter(s -> !s.contains(ex))
+                .reduce("", (a, b) -> a + b);
+    }
+}
