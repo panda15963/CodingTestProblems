48896f19609d8dee90ba5da3f830fd4f2a46d51f
Minseok Choi <seok7521@gmail.com>
2026-06-08 06:34:37 +0900

    [level 0] Title: 0 떼기, Time: 0.82 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String n_str) {
-        int idx = 0;
-        // 앞에서부터 0이 아닌 문자가 나올 때까지 이동
-        while (idx < n_str.length() && n_str.charAt(idx) == '0') {
-            idx++;
-        }
-        return n_str.substring(idx);
+        // 문자열을 정수로 변환 후 다시 문자열로 변환하여 0을 제거
+        return "" + Integer.parseInt(n_str);
     }
 }
