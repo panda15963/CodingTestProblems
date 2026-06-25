772807c419302d6927bc436f8659536ac40578d0
Minseok Choi <seok7521@gmail.com>
2026-06-26 06:02:22 +0900

    [level 3] Title: n + 1 카드게임, Time: 15.96 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,59 +1,85 @@
 import java.util.*;
 
 class Solution {
-    private Set<Integer> myCards = new HashSet<>();
-    private Set<Integer> tempCards = new HashSet<>();
-    private int pair;
-    private int round = 1;
-    private int myCoin;
-    private int n;
-
     public int solution(int coin, int[] cards) {
-        this.myCoin = coin;
-        this.n = cards.length;
+        int n = cards.length;
+        int initCount = n / 3;
 
-        for (int i = 0; i < n / 3; i++) { // (1)
+        Set<Integer> myCards = new HashSet<>();
+        Set<Integer> keepCards = new HashSet<>();
+
+        // 1. 초기 카드 설정
+        for (int i = 0; i < initCount; i++) {
             myCards.add(cards[i]);
         }
 
-        for (int card : myCards) { // (2)
-            if (myCards.contains(n + 1 - card)) {
-                pair++;
+        int currentCoin = coin;
+        int round = 1;
+        int deckIdx = initCount;
+
+        while (deckIdx < n) {
+            // 매 라운드 카드 2장 뽑기
+            int draw1 = cards[deckIdx++];
+            int draw2 = cards[deckIdx++];
+
+            keepCards.add(draw1);
+            keepCards.add(draw2);
+
+            boolean advanced = false;
+
+            // 1. 내 카드 두 장 사용 (코인 0개)
+            for (Integer card : new HashSet<>(myCards)) {
+                int target = (n + 1) - card;
+
+                if (card != target && myCards.contains(target)) {
+                    myCards.remove(card);
+                    myCards.remove(target);
+                    advanced = true;
+                    break;
+                }
             }
-        }
-        pair /= 2;
 
-        for (int i = n / 3; i < n; i += 2) { // (3)
-            matchCard(cards[i]); // (4)
-            matchCard(cards[i + 1]);
+            // 2. 내 카드 + 보관 카드 (코인 1개)
+            if (!advanced) {
+                for (Integer myCard : new HashSet<>(myCards)) {
+                    int target = (n + 1) - myCard;
 
-            if (pair < 1 && myCoin > 1) { // (5)
-                for (int card : tempCards) {
-                    if (tempCards.contains(n + 1 - card)) {
-                        pair++;
-                        myCoin -= 2;
-                        tempCards.remove(card); // (6)
+                    if (keepCards.contains(target) && currentCoin >= 1) {
+                        myCards.remove(myCard);
+                        keepCards.remove(target);
+                        currentCoin--;
+                        advanced = true;
                         break;
                     }
                 }
             }
 
-            if (pair < 1) { // (7)
+            // 3. 보관 카드 두 장 (코인 2개)
+            if (!advanced) {
+                for (Integer keepCard : new HashSet<>(keepCards)) {
+                    int target = (n + 1) - keepCard;
+
+                    if (keepCard != target &&
+                        keepCards.contains(target) &&
+                        currentCoin >= 2) {
+
+                        keepCards.remove(keepCard);
+                        keepCards.remove(target);
+                        currentCoin -= 2;
+                        advanced = true;
+                        break;
+                    }
+                }
+            }
+
+            // 4. 진행 불가
+            if (!advanced) {
                 break;
             }
+
             round++;
-            pair--;
         }
 
         return round;
-    }
-
-    private void matchCard(int card) {
-        if (myCoin > 0 && myCards.contains(n + 1 - card)) { // (8)
-            myCoin--;
-            pair++;
-            return;
-        }
-        tempCards.add(card); // (9)
     }
 }
