9e4fc5c873a269328052abe87ec554c493cc05d0
Minseok Choi <seok7521@gmail.com>
2026-06-25 13:27:29 +0900

    [level 2] Title: 가장 큰 수, Time: 121.41 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 139 MB, 시간: 160.29 ms
+메모리: 129 MB, 시간: 121.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 27일 06:29:33
+2026년 06월 25일 13:27:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,16 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
+        String[] strNums = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+            strNums[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(arr, (String a, String b) -> {
-            return (b + a).compareTo(a + b);
-        });
-        StringBuilder sb = new StringBuilder();
-        for (String s : arr) sb.append(s);
-        String answer = sb.toString();
-        return answer.replaceAll("^0+", "").isEmpty() ? "0" : answer.replaceAll("^0+", "");
+        
+        // 두 문자열을 결합하여 더 큰 값이 나오는 순서대로 정렬
+        Arrays.sort(strNums, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        // 첫 번째 숫자가 '0'이면 모든 숫자가 0이라는 뜻
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for (String num : strNums) {
+            answer.append(num);
+        }
+        
+        return answer.toString();
     }
 }
