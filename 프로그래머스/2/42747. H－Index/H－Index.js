7ec879dbc35a2f78931fe76f39c711c89e879a36
Minseok Choi <seok7521@gmail.com>
2026-06-25 13:28:26 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,7 +1,17 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a);
+    // 오름차순 정렬
+    citations.sort((a, b) => a - b);
+
+    let answer = 0;
+
     for (let i = 0; i < citations.length; i++) {
-        if (citations[i] < i + 1) return i;
+        const h = citations.length - i;
+
+        if (citations[i] >= h) {
+            answer = h;
+            break;
+        }
     }
-    return citations.length;
+
+    return answer;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 4.14 ms
+메모리: 44.1 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 25일 13:28:07
+2026년 06월 25일 13:28:23
 
 ### 문제 설명
 
