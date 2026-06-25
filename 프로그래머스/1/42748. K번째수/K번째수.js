005b176559b5fdded39971aa0cd33492676ddcdb
Minseok Choi <seok7521@gmail.com>
2026-06-25 13:26:31 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,14 @@
 function solution(array, commands) {
-    return commands.map(([i, j, k]) => {
-        const sub = array.slice(i-1, j).sort((a, b) => a - b);
-        return sub[k-1];
-    });
+    var answer = [];
+    for (let i = 0; i < commands.length; i++) {
+        let command = commands[i];
+        let start = command[0] - 1;
+        let end = command[1];
+        let k = command[2] - 1;
+        
+        // 배열 자르고 오름차순 정렬
+        let sliced = array.slice(start, end).sort((a, b) => a - b);
+        answer.push(sliced[k]);
+    }
+    return answer;
 }
