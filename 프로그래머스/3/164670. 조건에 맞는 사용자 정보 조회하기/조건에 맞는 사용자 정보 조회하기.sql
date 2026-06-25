517a30f5d2a7de064b3d8c33a5c39381c517a0c7
Minseok Choi <seok7521@gmail.com>
2026-06-25 13:14:10 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,25 @@
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    CONCAT(
+        U.CITY, ' ',
+        U.STREET_ADDRESS1, ' ',
+        U.STREET_ADDRESS2
+    ) AS '전체주소',
+    CONCAT(
+        LEFT(U.TLNO, 3), '-',
+        MID(U.TLNO, 4, 4), '-',
+        RIGHT(U.TLNO, 4)
+    ) AS '전화번호'
+FROM USED_GOODS_USER U
+JOIN USED_GOODS_BOARD B
+    ON U.USER_ID = B.WRITER_ID
+GROUP BY
+    U.USER_ID,
+    U.NICKNAME,
+    U.CITY,
+    U.STREET_ADDRESS1,
+    U.STREET_ADDRESS2,
+    U.TLNO
+HAVING COUNT(B.BOARD_ID) >= 3
+ORDER BY U.USER_ID DESC;
