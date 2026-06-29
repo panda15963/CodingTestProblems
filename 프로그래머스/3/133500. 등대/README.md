7466b914326cfdd6174c2a51cf22490507713846
Minseok Choi <seok7521@gmail.com>
2026-06-30 06:16:56 +0900

    [level 3] Title: 등대, Time: 84.04 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/README.md
+++ 프로그래머스/3/133500. 등대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등대 - 133500 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500?language=javascript) 
 
 ### 성능 요약
 
-메모리: 132 MB, 시간: 44.07 ms
+메모리: 144 MB, 시간: 84.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 30일 06:15:58
+2026년 06월 30일 06:16:53
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/133500. 등대/등대.js
@@ -0,0 +1,35 @@
+function solution(n, lighthouse) {
+  const memo = new Array(n + 1).fill(false);
+  let result = 0;
+
+  while(lighthouse.length) {      
+    const map = new Array(n + 1).fill().map(_ => []);
+
+    for (const el of lighthouse) {
+      const [a, b] = el;
+
+      map[a].push(b);
+      map[b].push(a);
+    }
+
+    map.filter(el => el.length === 1).forEach((el) => {
+        const target = el[0];
+        if (!memo[el[0]]) {
+            memo[el[0]] = true;
+            if (map[target].length !== 1) {
+                result += 1;
+            } else {
+                result += 0.5;
+            } 
+        }
+    });
+
+    lighthouse = lighthouse.filter((el) => {
+      const [a, b] = el;
+
+      return !memo[a] && !memo[b];
+    });
+  }
+
+  return result;
+}
