28080da0c536c48f340b1d038d6dfd0ed167822a
Minseok Choi <seok7521@gmail.com>
2026-07-14 06:43:00 +0900

    Time: 272 ms (16.41%), Space: 62.6 MB (80.93%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -0,0 +1,41 @@
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    if (lists === null || lists.length === 0) {
+        return null;
+    }
+
+    for (let i = 0; i < lists.length - 1; i++) {
+        lists[i + 1] = mergeTwoLists(lists[i], lists[i + 1]);
+    }
+
+    return lists[lists.length - 1];
+}
+
+function mergeTwoLists(
+    l1: ListNode | null,
+    l2: ListNode | null
+): ListNode | null {
+    const dummy = new ListNode(0);
+    let current: ListNode = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+
+        current = current.next;
+    }
+
+    if (l1 !== null) {
+        current.next = l1;
+    }
+
+    if (l2 !== null) {
+        current.next = l2;
+    }
+
+    return dummy.next;
+}
