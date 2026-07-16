4d9eec97699aecd0d3e92afe91595c6431ab5f73
Minseok Choi <seok7521@gmail.com>
2026-07-16 23:05:00 +0900

    Time: 125 ms (100%), Space: 81.8 MB (90.91%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.js
@@ -0,0 +1,31 @@
+function gcdSum(nums) {
+    const n = nums.length;
+    const prefixGcd = new Array(n);
+    let mx = 0;
+
+    for (let i = 0; i < n; i++) {
+        const x = nums[i];
+        mx = Math.max(mx, x);
+        prefixGcd[i] = gcd(x, mx);
+    }
+
+    prefixGcd.sort((a, b) => a - b);
+
+    let ans = 0;
+
+    for (let i = 0; i < (n >> 1); i++) {
+        ans += gcd(prefixGcd[i], prefixGcd[n - i - 1]);
+    }
+
+    return ans;
+}
+
+function gcd(a, b) {
+    while (b !== 0) {
+        const t = a % b;
+        a = b;
+        b = t;
+    }
+
+    return a;
+}
