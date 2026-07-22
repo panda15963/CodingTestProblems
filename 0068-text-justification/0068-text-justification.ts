4e49d6a87cd624489ab771be3d8967a22dc58996
Minseok Choi <seok7521@gmail.com>
2026-07-22 22:14:52 +0900

    Time: 1 ms (31.3%), Space: 52.5 MB (100%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.ts
@@ -0,0 +1,52 @@
+function fullJustify(words: string[], maxWidth: number): string[] {
+    const res: string[] = [];
+    const line: string[] = [];
+
+    let wordsLen = 0;
+
+    for (const word of words) {
+        const fullLen = wordsLen + line.length - 1;
+
+        if (fullLen + word.length + 1 > maxWidth) {
+            const spaces = Math.floor(
+                (maxWidth - wordsLen) / Math.max(1, line.length - 1)
+            );
+            let remain =
+                (maxWidth - wordsLen) % Math.max(1, line.length - 1);
+
+            let lineStr = "";
+
+            for (let j = 0; j < line.length; j++) {
+                lineStr += line[j];
+
+                if (line.length === 1) {
+                    lineStr += " ".repeat(spaces);
+                }
+
+                if (j < line.length - 1) {
+                    let spaceToAppend = spaces;
+
+                    if (remain > 0) {
+                        spaceToAppend++;
+                        remain--;
+                    }
+
+                    lineStr += " ".repeat(spaceToAppend);
+                }
+            }
+
+            res.push(lineStr);
+            line.length = 0;
+            wordsLen = 0;
+        }
+
+        line.push(word);
+        wordsLen += word.length;
+    }
+
+    const lastLine = line.join(" ");
+    const lastRowSpace = maxWidth - lastLine.length;
+    res.push(lastLine + " ".repeat(lastRowSpace));
+
+    return res;
+}
