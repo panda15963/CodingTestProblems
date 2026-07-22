76c59b0efc902de13ff4ddb1dd4478a923cd9603
Minseok Choi <seok7521@gmail.com>
2026-07-23 07:23:46 +0900

    Time: 11 ms (72.73%), Space: 58.8 MB (93.33%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.ts
@@ -0,0 +1,64 @@
+function solveNQueens(n: number): string[][] {
+    const ret: string[][] = [];
+    const queens: number[][] = [];
+
+    recall(n, queens, 0);
+
+    return ret;
+
+    function recall(n: number, queens: number[][], row: number): void {
+        // 모든 행에 퀸을 배치한 경우
+        if (row >= n && queens.length === n) {
+            ret.push(getStrings(n, queens));
+            return;
+        }
+
+        // 현재 행의 모든 열 탐색
+        for (let col = 0; col < n; col++) {
+            if (isPosable(queens, row, col)) {
+                queens.push([row, col]);
+                recall(n, queens, row + 1);
+                queens.pop(); // 백트래킹
+            }
+        }
+    }
+
+    function getStrings(n: number, queens: number[][]): string[] {
+        const result: string[] = [];
+
+        for (let row = 0; row < queens.length; row++) {
+            const queen = queens[row];
+            let str = "";
+
+            for (let col = 0; col < n; col++) {
+                if (queen[0] === row && queen[1] === col) {
+                    str += "Q";
+                } else {
+                    str += ".";
+                }
+            }
+
+            result.push(str);
+        }
+
+        return result;
+    }
+
+    function isPosable(
+        queens: number[][],
+        row: number,
+        col: number
+    ): boolean {
+        for (const queen of queens) {
+            if (
+                queen[1] === col ||
+                queen[0] + queen[1] === row + col ||
+                queen[0] - queen[1] === row - col
+            ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
