3b7b41739037f4170bd9d36e46d6f5373ff64525
Minseok Choi <seok7521@gmail.com>
2026-07-23 22:11:22 +0900

    Time: 0 ms (100%), Space: 72.8 MB (100%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var uniqueXorTriplets = function (nums) {
+    const n = nums.length;
+
+    if (n < 3) {
+        return n;
+    }
+
+    const x = Math.floor(Math.log(n) / Math.log(2));
+
+    return 1 << (x + 1);
+};
