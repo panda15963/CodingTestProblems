1ebb8db8f14f2b9fec687b6a915c99c51cb0c07d
Minseok Choi <seok7521@gmail.com>
2026-07-27 07:19:03 +0900

    Time: 4 ms (63.04%), Space: 50.5 MB (10.6%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int[][] minCost2d = new int[rows][cols];
+
+        // 초기화
+        minCost2d[0][0] = grid[0][0];
+
+        for (int col = 1; col < cols; col++) {
+            minCost2d[0][col] = grid[0][col] + minCost2d[0][col - 1];
+        }
+
+        for (int row = 1; row < rows; row++) {
+            minCost2d[row][0] = grid[row][0] + minCost2d[row - 1][0];
+        }
+
+        // Bottom-Up DP
+        for (int row = 1; row < rows; row++) {
+            for (int col = 1; col < cols; col++) {
+                int upCost = minCost2d[row - 1][col];
+                int leftCost = minCost2d[row][col - 1];
+
+                minCost2d[row][col] =
+                        Math.min(upCost, leftCost) + grid[row][col];
+            }
+        }
+
+        return minCost2d[rows - 1][cols - 1];
+    }
+}
