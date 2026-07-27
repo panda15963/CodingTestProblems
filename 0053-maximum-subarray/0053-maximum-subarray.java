2b3c8f82048a09b7654036092c7268dcde01921c
Minseok Choi <seok7521@gmail.com>
2026-07-27 21:52:33 +0900

    Time: 1 ms (99.95%), Space: 80.8 MB (5.77%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int subMax = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            subMax = Math.max(subMax + nums[i], nums[i]);
+            ans = Math.max(subMax, ans);
+        }
+        return ans;
+    }
+}
