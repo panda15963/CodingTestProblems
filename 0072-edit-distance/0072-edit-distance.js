510321962df8e45e5af9fc60edc9d716227ef358
Minseok Choi <seok7521@gmail.com>
2026-07-29 07:25:27 +0900

    Time: 12 ms (31.43%), Space: 58.4 MB (78.82%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+function minDistance(word1, word2) {
+    const row = word1.length;
+    const col = word2.length;
+
+    const dp = Array.from({ length: row + 1 }, () =>
+        Array(col + 1).fill(0)
+    );
+
+    for (let i = 1; i <= row; i++) {
+        dp[i][0] = i;
+    }
+
+    for (let j = 1; j <= col; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= row; i++) {
+        for (let j = 1; j <= col; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] =
+                    Math.min(
+                        dp[i - 1][j],
+                        dp[i - 1][j - 1],
+                        dp[i][j - 1]
+                    ) + 1;
+            }
+        }
+    }
+
+    return dp[row][col];
+}
