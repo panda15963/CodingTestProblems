ae9b0cd13ce407822b2e18b7bbc29e418b7db2ea
Minseok Choi <seok7521@gmail.com>
2026-07-28 21:19:09 +0900

    Time: 1 ms (86.74%), Space: 59.1 MB (56.16%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canJump(nums) {
+    let maxReach = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        // 현재 인덱스에 도달할 수 없는 경우
+        if (i > maxReach) {
+            return false;
+        }
+
+        // 현재 위치를 이용해 도달 가능한 최대 범위를 갱신
+        maxReach = Math.max(maxReach, i + nums[i]);
+    }
+
+    return true;
+}
