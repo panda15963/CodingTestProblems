7bcb8979d7e61a5fbd979f370ad1c278f682b772
Minseok Choi <seok7521@gmail.com>
2026-07-29 21:01:39 +0900

    Time: 3 ms (70.25%), Space: 59.5 MB (85.34%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void}
+ */
+function setZeroes(matrix) {
+    const row = matrix.length;
+    const col = matrix[0].length;
+
+    let firstRowZero = false;
+    let firstColZero = false;
+
+    // 첫 열 확인
+    for (let i = 0; i < row; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+
+    // 첫 행 확인
+    for (let j = 0; j < col; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    // 마킹
+    for (let i = 1; i < row; i++) {
+        for (let j = 1; j < col; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // 내부 0 처리
+    for (let i = 1; i < row; i++) {
+        for (let j = 1; j < col; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // 첫 열
+    if (firstColZero) {
+        for (let i = 0; i < row; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    // 첫 행
+    if (firstRowZero) {
+        for (let j = 0; j < col; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+}
