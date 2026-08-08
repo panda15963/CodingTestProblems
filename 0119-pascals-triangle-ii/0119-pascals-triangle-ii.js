622b4a23ec9eb5ad46cd14558d262bba9af48c43
Minseok Choi <seok7521@gmail.com>
2026-08-08 12:54:40 +0900

    Time: 1 ms (41.76%), Space: 53.2 MB (78.43%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,27 @@
+var getRow = function (rowIndex) {
+    if (rowIndex === 0) {
+        return [1];
+    }
+
+    if (rowIndex === 1) {
+        return [1, 1];
+    }
+
+    let prev = [1, 1];
+
+    for (let i = 2; i <= rowIndex; i++) {
+        const cur = [];
+
+        cur.push(1);
+
+        for (let j = 1; j < i; j++) {
+            cur.push(prev[j - 1] + prev[j]);
+        }
+
+        cur.push(1);
+
+        prev = cur;
+    }
+
+    return prev;
+};
