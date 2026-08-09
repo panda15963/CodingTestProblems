fee6caf555e662e373856389cb6d926b6873b58f
Minseok Choi <seok7521@gmail.com>
2026-08-09 09:34:22 +0900

    Time: 55 ms (94.75%), Space: 66.2 MB (76.93%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,11 @@
+var getIntersectionNode = function (headA, headB) {
+    let pointerA = headA;
+    let pointerB = headB;
+
+    while (pointerA !== pointerB) {
+        pointerA = pointerA ? pointerA.next : headB;
+        pointerB = pointerB ? pointerB.next : headA;
+    }
+
+    return pointerA;
+};
