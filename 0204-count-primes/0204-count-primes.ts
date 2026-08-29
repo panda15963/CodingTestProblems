885c3cc66a7a0fc9db774b505ec0e24b01a8288c
Minseok Choi <seok7521@gmail.com>
2026-08-29 09:18:42 +0900

    Time: 1074 ms (25.27%), Space: 95.9 MB (58.24%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.ts
@@ -0,0 +1,28 @@
+function countPrimes(n: number): number {
+    if (n < 3) {
+        return 0;
+    }
+
+    if (n <= 3) {
+        return 1;
+    }
+
+    const notPrime: boolean[] = new Array(n).fill(false);
+
+    // 2는 소수이므로 미리 1개 카운트
+    let count: number = 1;
+
+    // 홀수만 검사
+    for (let i: number = 3; i < n; i += 2) {
+        if (notPrime[i] === false) {
+            count++;
+
+            // i의 홀수 배수들을 소수가 아닌 것으로 표시
+            for (let j: number = 3; i * j < n; j += 2) {
+                notPrime[i * j] = true;
+            }
+        }
+    }
+
+    return count;
+}
