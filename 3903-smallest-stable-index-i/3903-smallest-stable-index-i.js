bc63605b6178fac464e26e169416323b33418312
Minseok Choi <seok7521@gmail.com>
2026-09-04 22:13:22 +0900

    Time: 0 ms (100%), Space: 56.6 MB (62.96%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
@@ -0,0 +1,22 @@
+function firstStableIndex(nums, k) {
+    const n = nums.length;
+    const right = new Array(n);
+
+    right[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        right[i] = Math.min(right[i + 1], nums[i]);
+    }
+
+    let left = 0;
+
+    for (let i = 0; i < n; i++) {
+        left = Math.max(left, nums[i]);
+
+        if (left - right[i] <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+}
