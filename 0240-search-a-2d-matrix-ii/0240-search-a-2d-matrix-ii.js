f0100d05dbbfc258b51643271729b6f96cb7acd1
Minseok Choi <seok7521@gmail.com>
2026-09-08 07:05:32 +0900

    Time: 1974 ms (9.87%), Space: 59.1 MB (22.6%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,21 @@
+function searchMatrix(matrix, target) {
+    for (const row of matrix) {
+        let left = 0;
+        let right = row.length - 1;
+
+        // 각 행에서 이진 탐색
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+
+            if (row[mid] === target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return false;
+}
