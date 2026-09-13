c10fa5fb6837059634164343b550d367ae2364e0
Minseok Choi <seok7521@gmail.com>
2026-09-13 15:35:59 +0900

    Time: 81 ms (67.6%), Space: 69.1 MB (24.03%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,67 @@
+// 직렬화
+var serialize = function (root) {
+    if (root === null) {
+        return "# #";
+    }
+
+    const queue = [root];
+    const result = ["#"];
+
+    let index = 0;
+
+    // BFS
+    while (index < queue.length) {
+        const node = queue[index++];
+
+        if (node !== null) {
+            queue.push(node.left);
+            queue.push(node.right);
+
+            result.push(String(node.val));
+        } else {
+            result.push("#");
+        }
+    }
+
+    return result.join(" ");
+};
+
+
+// 역직렬화
+var deserialize = function (data) {
+    if (data === "# #") {
+        return null;
+    }
+
+    const nodes = data.split(" ");
+
+    const root = new TreeNode(Number(nodes[1]));
+
+    const queue = [root];
+
+    let queueIndex = 0;
+    let index = 2;
+
+    // BFS
+    while (queueIndex < queue.length) {
+        const node = queue[queueIndex++];
+
+        // 왼쪽 자식
+        if (nodes[index] !== "#") {
+            node.left = new TreeNode(Number(nodes[index]));
+            queue.push(node.left);
+        }
+
+        index++;
+
+        // 오른쪽 자식
+        if (nodes[index] !== "#") {
+            node.right = new TreeNode(Number(nodes[index]));
+            queue.push(node.right);
+        }
+
+        index++;
+    }
+
+    return root;
+};
