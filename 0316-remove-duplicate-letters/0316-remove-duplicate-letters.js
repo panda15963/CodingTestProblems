a40057e2fea7d988567bd107e02e2521732c2100
Minseok Choi <seok7521@gmail.com>
2026-09-16 22:03:20 +0900

    Time: 3 ms (74.38%), Space: 57.5 MB (24.56%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -0,0 +1,33 @@
+var removeDuplicateLetters = function(s) {
+    const counter = new Array(26).fill(0);
+
+    for (const c of s) {
+        counter[c.charCodeAt(0) - 97]++;
+    }
+
+    const stack = [];
+    const visited = new Array(26).fill(false);
+
+    for (const c of s) {
+        const index = c.charCodeAt(0) - 97;
+        counter[index]--;
+
+        if (visited[index]) {
+            continue;
+        }
+
+        while (
+            stack.length > 0 &&
+            c < stack[stack.length - 1] &&
+            counter[stack[stack.length - 1].charCodeAt(0) - 97] > 0
+        ) {
+            const top = stack.pop();
+            visited[top.charCodeAt(0) - 97] = false;
+        }
+
+        stack.push(c);
+        visited[index] = true;
+    }
+
+    return stack.join("");
+};
