c3160635405c2b29793b65f6dd48fe6bbcbbae80
Minseok Choi <seok7521@gmail.com>
2026-09-17 21:13:54 +0900

    Time: 33 ms (74.12%), Space: 62.2 MB (56.24%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.ts
@@ -0,0 +1,14 @@
+function coinChange(coins: number[], amount: number): number {
+    const dp: number[] = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let i = 1; i <= amount; i++) {
+        for (const coin of coins) {
+            if (coin <= i) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    return dp[amount] === Infinity ? -1 : dp[amount];
+}
