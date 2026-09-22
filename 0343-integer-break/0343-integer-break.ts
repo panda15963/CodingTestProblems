f8295e8b9891e3c6f0dd09abb12ce79c6c83d41c
Minseok Choi <seok7521@gmail.com>
2026-09-22 22:21:01 +0900

    Time: 1 ms (46.43%), Space: 55.7 MB (10.71%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.ts
@@ -0,0 +1,17 @@
+function integerBreak(n: number): number {
+    const dp: number[] = new Array(n + 1).fill(0);
+
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j < i; j++) {
+            dp[i] = Math.max(
+                dp[i],
+                j * (i - j),
+                j * dp[i - j]
+            );
+        }
+    }
+
+    return dp[n];
+}
